90fe69b9a5c7093fe15257fe2c351110ffdd8a56
root <root@Vlamir-ubuntu.u2hkazgmscwuro3xnzsw5hchah.parx.internal.cloudapp.net>
2021-06-07 19:49:40 +0000

    p12

---
+++ query1.sql
@@ -0,0 +1,14 @@
+#Insert
+INSERT INTO public.customers VALUES (20001, 'Sergi', 'González', '6224597470 Dell Way', NULL, 'DVCINXG', NULL, 0, 'Australia', 2, 'sg@dell.com', '6224597470', 3, '1869697669055999', '2010/07', 'user20001', 'password', 17, 40000, 'F');
+INSERT INTO public.customers VALUES (20002, 'Pep', 'López', '6224597470 Dell Way', NULL, 'DVCINXG', NULL, 0, 'Australia', 2, 'sg@dell.com', '6224597470', 3, '1869697669055999', '2010/07', 'user20002', 'password', 17, 40000, 'F');
+
+#SQL
+create or replace function del_under18()
+returns setof integer as $$
+delete from customers where age < 18
+returning customerid
+$$ language sql;
+
+#select
+select del_under18();
+
